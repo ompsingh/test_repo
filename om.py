31e423d73828b078adbfe95c6fc32326c41ad529
ompsingh <ompsingh41002@gmail.com>
2020-06-24 20:28:39 -0400

    v1

--- om.py
+++ om.py
@@ -1,1 +1,10 @@
 print("This is test # github.atom.io/login")
+
+# $ git commit -m 'first commit'
+#
+# *** Please tell me who you are.
+#
+# Run
+#
+#   git config --global user.email "you@example.com"
+#   git config --global user.name "Your Name"
